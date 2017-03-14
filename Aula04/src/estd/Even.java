16f39961002aef4e40907eb535cdde74d1920517
Douglas steuernagel <douglas.steuernagel@univille.br>
2017-03-13 21:42:32 -0300

    Aula 04 - 21:42

--- Aula04/src/estd/Even.java
+++ Aula04/src/estd/Even.java
@@ -3,15 +3,20 @@ package estd;
 public class Even {
 
 	public int countEvens(int[] nums){
-		for(int i=0; i<nums.length; i++){
+		/*for(int i=0; i<nums.length; i++){
 			System.out.println(nums[i]);
+		}*/
+		int counter = 0;
+		
+		for(int num : nums){
+			if(num % 2 == 0){
+				//é par
+				counter++;
+				
+			}
 		}
 		
-		for(int i : nums){
-			System.out.println(i);
-		}
-		
-		return 0;
+		return counter;
 	}
 	
 }
